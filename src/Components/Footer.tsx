ea0b1b08685907cca83b04be78af62dac36b3613
Dionisio-DM <dionisiodiogenes@hotmail.com>
2025-02-12 17:42:08 -0300

    feat: inicialização componente footer

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,18 @@
+import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
+import { Flex, Link } from "@radix-ui/themes";
+
+export const Footer: React.FC = () => {
+  return (
+    <Flex align={"center"} justify={"center"} direction={"row"} gap={"4"}>
+      <Link href="https://github.com/Dionisio-DM" target="_blank">
+        <GitHubLogoIcon width={"1.5rem"} height={"1.5rem"} />
+      </Link>
+      <Link
+        href="https://www.linkedin.com/in/dionisiodiogenes/"
+        target="_blank"
+      >
+        <LinkedInLogoIcon width={"1.5rem"} height={"1.5rem"} />
+      </Link>
+    </Flex>
+  );
+};
