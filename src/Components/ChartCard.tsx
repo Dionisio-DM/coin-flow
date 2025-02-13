f67f69d2eac6206a84a51a32b1999afd5e124e8c
Dionisio-DM <dionisiodiogenes@hotmail.com>
2025-02-12 23:12:48 -0300

    fix: overflow consertado com inclusão do scroll area

--- src/Components/ChartCard.tsx
+++ src/Components/ChartCard.tsx
@@ -1,4 +1,4 @@
-import { Card, SegmentedControl } from "@radix-ui/themes";
+import { Card, ScrollArea, SegmentedControl } from "@radix-ui/themes";
 import {
   CartesianGrid,
   Line,
@@ -55,13 +55,19 @@ const data = [
 export const ChartCard: React.FC = () => {
   return (
     <Card>
-      <SegmentedControl.Root defaultValue="inbox" mb={"5"} mt={"2"} ml={"2"}>
-        <SegmentedControl.Item value="month">1 mês</SegmentedControl.Item>
-        <SegmentedControl.Item value="semester">6 meses</SegmentedControl.Item>
-        <SegmentedControl.Item value="year">1 ano</SegmentedControl.Item>
-        <SegmentedControl.Item value="halfDecade">5 ano</SegmentedControl.Item>
-        <SegmentedControl.Item value="max">max</SegmentedControl.Item>
-      </SegmentedControl.Root>
+      <ScrollArea scrollbars="horizontal">
+        <SegmentedControl.Root defaultValue="inbox" mb={"5"} mt={"2"} ml={"2"}>
+          <SegmentedControl.Item value="month">1 mês</SegmentedControl.Item>
+          <SegmentedControl.Item value="semester">
+            6 meses
+          </SegmentedControl.Item>
+          <SegmentedControl.Item value="year">1 ano</SegmentedControl.Item>
+          <SegmentedControl.Item value="halfDecade">
+            5 ano
+          </SegmentedControl.Item>
+          <SegmentedControl.Item value="max">max</SegmentedControl.Item>
+        </SegmentedControl.Root>
+      </ScrollArea>
 
       <ResponsiveContainer width={"100%"} height={300}>
         <LineChart
@@ -79,7 +85,7 @@ export const ChartCard: React.FC = () => {
           <CartesianGrid strokeDasharray="4 4" />
           <XAxis dataKey="name" tick={{ fill: "#ccc" }} tickMargin={8} />
 
-          <YAxis tick={{ fill: "#ccc" }} />
+          <YAxis tick={{ fill: "#ccc" }} tickMargin={8} />
           <Tooltip
             contentStyle={{
               backgroundColor: "#333",
