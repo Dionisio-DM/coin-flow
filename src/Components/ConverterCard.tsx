b58a3674fe38343ead7c74f4c9f7ba004ab3eb64
Dionisio-DM <dionisiodiogenes@hotmail.com>
2025-02-12 21:05:13 -0300

    feat: add label

--- src/Components/ConverterCard.tsx
+++ src/Components/ConverterCard.tsx
@@ -7,9 +7,15 @@ export const ConverterCard: React.FC = () => {
       <Box>
         <Flex gap={"3"} direction={"row"} width={"100%"} justify={"between"}>
           <Box width={"35rem"}>
+            <Text as="label" size={"1"}>
+              Quantia
+            </Text>
             <InputField></InputField>
           </Box>
           <Box width={"35rem"}>
+            <Text as="label" size={"1"}>
+              Converter para
+            </Text>
             <InputField></InputField>
           </Box>
         </Flex>
