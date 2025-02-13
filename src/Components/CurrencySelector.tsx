dc4c467433984602cf21ce2400f46f484baea0b8
Dionisio-DM <dionisiodiogenes@hotmail.com>
2025-02-13 11:34:33 -0300

    feat: add currencies e updt dinamico do valor do dropdown

--- src/Components/CurrencySelector.tsx
+++ src/Components/CurrencySelector.tsx
@@ -1,17 +1,76 @@
 import { Button, DropdownMenu } from "@radix-ui/themes";
+import { useState } from "react";
 
-export const CurrencySelector: React.FC = () => {
+interface CurrencySelectorProps {
+  initial: string;
+}
+
+const currencies = [
+  "AUD",
+  "BGN",
+  "BRL",
+  "CAD",
+  "CHF",
+  "CNY",
+  "CZK",
+  "DKK",
+  "EUR",
+  "GBP",
+  "HKD",
+  "HUF",
+  "IDR",
+  "ILS",
+  "INR",
+  "ISK",
+  "JPY",
+  "KRW",
+  "MXN",
+  "MYR",
+  "NOK",
+  "NZD",
+  "PHP",
+  "PLN",
+  "RON",
+  "SEK",
+  "SGD",
+  "THB",
+  "TRY",
+  "USD",
+  "ZAR",
+];
+
+export const CurrencySelector: React.FC<CurrencySelectorProps> = ({
+  initial,
+}) => {
+  const [currency, setCurrency] = useState(initial);
+
+  const handleSelect = (event: Event) => {
+    const target = event.currentTarget as HTMLElement;
+    const targetValue = target.dataset.value;
+    if (targetValue) setCurrency(targetValue);
+  };
+
   return (
     <DropdownMenu.Root>
       <DropdownMenu.Trigger>
-        <Button variant="ghost" color="gray">
-          BRL
+        <Button variant="ghost" color="gray" size={"3"}>
+          {currency}
           <DropdownMenu.TriggerIcon />
         </Button>
       </DropdownMenu.Trigger>
-      <DropdownMenu.Content size="1">
-        <DropdownMenu.Item>USD</DropdownMenu.Item>
-        <DropdownMenu.Item>EUR</DropdownMenu.Item>
+      <DropdownMenu.Content size="2">
+        {currencies.map((curr) => {
+          if (curr !== currency)
+            return (
+              <DropdownMenu.Item
+                key={curr}
+                data-value={curr}
+                onSelect={handleSelect}
+              >
+                {curr}
+              </DropdownMenu.Item>
+            );
+        })}
       </DropdownMenu.Content>
     </DropdownMenu.Root>
   );
