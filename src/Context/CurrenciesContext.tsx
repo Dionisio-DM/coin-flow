ec5a3d31403e24b0f32f7a882b0a17b5ad7cbbad
Dionisio-DM <dionisiodiogenes@hotmail.com>
2025-02-14 13:59:24 -0300

    feat: contexto para inicializar e atualizar dados do gráfico, conversor e infocards

---
+++ src/Context/CurrenciesContext.tsx
@@ -0,0 +1,109 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { currencyApi } from "../Services/currencyApi";
+import { RatePeriod, SeriesData } from "../Entities/currency";
+import { formatDate, fromDate } from "../Utils/dateOperations";
+
+export interface ContextData {
+  rate: number;
+  variationRate: number;
+  averageInPeriod: number;
+  seriesData: SeriesData[];
+  getRate: (baseCurrency: string, targetCurrency: string) => Promise<void>;
+  getSeriesData: (
+    fromDate: string,
+    baseCurrency: string,
+    targetCurrency: string
+  ) => Promise<SeriesData[]>;
+}
+
+export const CurrenciesContext = createContext({} as ContextData);
+
+interface CurrenciesContextProviderProps {
+  children: ReactNode;
+}
+
+export const CurrenciesContextProvider: React.FC<
+  CurrenciesContextProviderProps
+> = ({ children }) => {
+  const [rate, setRate] = useState<number>(1);
+  const [seriesData, setSeriesData] = useState<SeriesData[]>([]);
+  const [variationRate, setVariationRate] = useState<number>(0);
+  const [averageInPeriod, setAverageInPeriod] = useState<number>(1);
+
+  const parseRateInPeriodData = (data: RatePeriod) => {
+    const series: SeriesData[] = Object.entries(data.rates).map(
+      ([date, rateObj]) => ({
+        date: date,
+        price: +(1 / rateObj["USD"]).toFixed(2),
+      })
+    );
+
+    const calculatedVariationRate = +(
+      (series[series.length - 1].price / series[series.length - 2].price - 1) *
+      100
+    ).toFixed(3);
+
+    const sumInPeriod = series.reduce(
+      (accumulator, currentValue) => accumulator + currentValue.price,
+      0
+    );
+
+    const averageInPeriod = +(sumInPeriod / series.length).toFixed(3);
+
+    setSeriesData(series);
+    setVariationRate(calculatedVariationRate);
+    setAverageInPeriod(averageInPeriod);
+
+    return series;
+  };
+
+  // const formatDate = (date: Date)
+
+  useEffect(() => {
+    currencyApi
+      .getRate("BRL", "USD")
+      .then((data) => setRate(data.rates["USD"]));
+  }, []);
+
+  useEffect(() => {
+    const date = fromDate(1);
+    const formatedDate = formatDate(date);
+    currencyApi
+      .getRateInPeriod(formatedDate, "BRL", "USD")
+      .then(parseRateInPeriodData);
+  });
+
+  // adicionar useeffect para inicializar seriesdata, variationrate e averageinperiod
+
+  const getRate = async (baseCurrency: string, targetCurrency: string) => {
+    currencyApi
+      .getRate(baseCurrency, targetCurrency)
+      .then((data) => setRate(data.rates[targetCurrency]));
+  };
+
+  const getSeriesData = async (
+    // garantir atualização de seriesdata, variationrate e averageinperiod
+    fromDate: string,
+    baseCurrency: string,
+    targetCurrency: string
+  ) => {
+    return currencyApi
+      .getRateInPeriod(fromDate, baseCurrency, targetCurrency)
+      .then(parseRateInPeriodData);
+  };
+
+  return (
+    <CurrenciesContext.Provider
+      value={{
+        rate,
+        variationRate,
+        averageInPeriod,
+        seriesData,
+        getRate,
+        getSeriesData,
+      }}
+    >
+      {children}
+    </CurrenciesContext.Provider>
+  );
+};
