289d6abfe08114ab48eae53d3f49fd89a1ca700f
Dionisio-DM <dionisiodiogenes@hotmail.com>
2025-02-14 14:25:47 -0300

    feat: envolvendo app com context

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,32 @@ import { Footer } from "./Components/Footer";
 import { ConverterCard } from "./Components/ConverterCard";
 import { ChartCard } from "./Components/ChartCard";
 import { InfoCard } from "./Components/InfoCard";
+import { CurrenciesContextProvider } from "./Context/CurrenciesContext";
 
 function App() {
   return (
     <Theme appearance="dark">
-      <Box maxWidth={"80rem"} maxHeight={"100vh"} mx={"auto"}>
-        <Header />
-        <Box mt={"5"}>
-          <Box my={"0.85rem"}>
-            <ConverterCard></ConverterCard>
+      <CurrenciesContextProvider>
+        <Box maxWidth={"80rem"} maxHeight={"100vh"} mx={"auto"}>
+          <Header />
+          <Box mt={"5"}>
+            <Box my={"0.85rem"}>
+              <ConverterCard></ConverterCard>
+            </Box>
+            <Box my={"0.85rem"}>
+              <ChartCard></ChartCard>
+            </Box>
+            <Flex wrap={"wrap"} justify={"between"} gap={"4"}>
+              <InfoCard title="Taxa Atual" content="conteudo"></InfoCard>
+              <InfoCard title="Taxa Atual" content="conteudo"></InfoCard>
+              <InfoCard title="Taxa Atual" content="conteudo"></InfoCard>
+            </Flex>
           </Box>
-          <Box my={"0.85rem"}>
-            <ChartCard></ChartCard>
+          <Box my={"3rem"} pb={"6"}>
+            <Footer />
           </Box>
-          <Flex wrap={"wrap"} justify={"between"} gap={"4"}>
-            <InfoCard title="Taxa Atual" content="conteudo"></InfoCard>
-            <InfoCard title="Taxa Atual" content="conteudo"></InfoCard>
-            <InfoCard title="Taxa Atual" content="conteudo"></InfoCard>
-          </Flex>
         </Box>
-        <Box my={"3rem"} pb={"6"}>
-          <Footer />
-        </Box>
-      </Box>
+      </CurrenciesContextProvider>
     </Theme>
   );
 }
