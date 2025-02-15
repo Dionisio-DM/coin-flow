a2edf44a88a1e4c996a2e984f5684bb75716a896
Dionisio-DM <dionisiodiogenes@hotmail.com>
2025-02-15 12:11:02 -0300

    feat: lista de nomes de moedas vindo do context para o currencySelector

--- src/Components/CurrencySelector.tsx
+++ src/Components/CurrencySelector.tsx
@@ -1,47 +1,15 @@
 import { Button, DropdownMenu } from "@radix-ui/themes";
 import { useState } from "react";
+import { useCurrencies } from "../Hooks/useCurrencies";
 
 interface CurrencySelectorProps {
   initial: string;
 }
 
-const currencies = [
-  "AUD",
-  "BGN",
-  "BRL",
-  "CAD",
-  "CHF",
-  "CNY",
-  "CZK",
-  "DKK",
-  "EUR",
-  "GBP",
-  "HKD",
-  "HUF",
-  "IDR",
-  "ILS",
-  "INR",
-  "ISK",
-  "JPY",
-  "KRW",
-  "MXN",
-  "MYR",
-  "NOK",
-  "NZD",
-  "PHP",
-  "PLN",
-  "RON",
-  "SEK",
-  "SGD",
-  "THB",
-  "TRY",
-  "USD",
-  "ZAR",
-];
-
 export const CurrencySelector: React.FC<CurrencySelectorProps> = ({
   initial,
 }) => {
+  const { currencies } = useCurrencies();
   const [currency, setCurrency] = useState(initial);
 
   const handleSelect = (event: Event) => {
