4207e0744097fff0b0cd9025724d9ff68acf4b07
Dionisio-DM <dionisiodiogenes@hotmail.com>
2025-02-16 15:13:21 -0300

    feat: add func reutilizavel para inicializar e atualizar gráfico e informações correlatas

--- src/Context/ChartContext.tsx
+++ src/Context/ChartContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
-import { SeriesData } from "../Entities/currency";
+import { RatePeriod, SeriesData } from "../Entities/currency";
 import { formatDate, fromDate } from "../Utils/dateOperations";
 import { currencyApi } from "../Services/currencyApi";
 import {
@@ -42,21 +42,27 @@ export const ChartContextProvider: React.FC<ChartContextProviderProps> = ({
   const [maxInPeriod, setMaxInPeriod] = useState<number>(8);
   const [minInPeriod, setMinInPeriod] = useState<number>(1);
 
+  // função reutilizável para atualizar campos relevantes
+  const updateChartContext = (data: RatePeriod, targetCurrency: string) => {
+    const series = formatDateRatePeriod(data, targetCurrency);
+    const calculatedVariationRate = VariationRate(series);
+    const averageInPeriod = average(series);
+
+    const { minPrice, maxPrice } = minMaxExtractor(series);
+    setSeriesData(series);
+    setVariationRate(calculatedVariationRate);
+    setAverageInPeriod(averageInPeriod);
+    setMaxInPeriod(maxPrice);
+    setMinInPeriod(minPrice);
+    return series;
+  };
+
   // Inicialização gráfico, variação diária e média no período
   useEffect(() => {
     const date = fromDate(1);
     const formatedDate = formatDate(date);
     currencyApi.getRateInPeriod(formatedDate, "BRL", "USD").then((data) => {
-      const series = formatDateRatePeriod(data, "USD");
-      const calculatedVariationRate = VariationRate(series);
-      const averageInPeriod = average(series);
-
-      const { minPrice, maxPrice } = minMaxExtractor(series);
-      setSeriesData(series);
-      setVariationRate(calculatedVariationRate);
-      setAverageInPeriod(averageInPeriod);
-      setMaxInPeriod(maxPrice);
-      setMinInPeriod(minPrice);
+      return updateChartContext(data, "USD");
     });
   }, []);
 
@@ -75,17 +81,7 @@ export const ChartContextProvider: React.FC<ChartContextProviderProps> = ({
     return currencyApi
       .getRateInPeriod(formatedFromDate, baseCurrency, targetCurrency)
       .then((data) => {
-        const series = formatDateRatePeriod(data, targetCurrency);
-        const calculatedVariationRate = VariationRate(series);
-        const averageInPeriod = average(series);
-
-        const { minPrice, maxPrice } = minMaxExtractor(series);
-        setSeriesData(series);
-        setVariationRate(calculatedVariationRate);
-        setAverageInPeriod(averageInPeriod);
-        setMaxInPeriod(maxPrice);
-        setMinInPeriod(minPrice);
-        return series;
+        return updateChartContext(data, targetCurrency);
       });
   };
 
